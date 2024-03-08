f0afbd95c87d80e01c4c31823f3ac38325bc6afb
Nicodav28 <esteveznicolas0@gmail.com>
2024-03-08 12:53:54 -0500

    General adjustments

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
+  console.log('Console log from a container.');
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(
     new ValidationPipe({
