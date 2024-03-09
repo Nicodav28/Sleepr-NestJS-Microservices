1a742956d88ef0b808d6d845e8c101bb6b35bf44
Nicodav28 <esteveznicolas0@gmail.com>
2024-03-09 18:49:48 -0500

    Initialize the authentication microservice by initializating the users module

---
+++ apps/auth/src/users/userts.repository.ts
@@ -0,0 +1,14 @@
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { UserDocument } from './models/users.schema';
+
+@Injectable()
+export class UsersRepository extends AbstractRepository<UserDocument> {
+  protected readonly logger = new Logger(UsersRepository.name);
+
+  constructor(@InjectModel(UserDocument.name) userModel: Model<UserDocument>) {
+    super(userModel);
+  }
+}
--- apps/reservations/src/reservations.repository.ts
+++ apps/reservations/src/reservations.repository.ts
@@ -1,18 +1,17 @@
-import { AbstractRepository } from "@app/common/database/abstract.repository";
-import { ReservationDocument } from "./models/reservation.schema";
-import { Injectable, Logger } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-
-
+import { AbstractRepository } from '@app/common/database/abstract.repository';
+import { ReservationDocument } from './models/reservation.schema';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ReservationsRepository extends AbstractRepository<ReservationDocument>
-{
-    protected readonly logger = new Logger(ReservationsRepository.name);
+export class ReservationsRepository extends AbstractRepository<ReservationDocument> {
+  protected readonly logger = new Logger(ReservationsRepository.name);
 
-    constructor(@InjectModel(ReservationDocument.name) reservationModule: Model<ReservationDocument>){
-        super(reservationModule)
-    }
-    
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
 }
