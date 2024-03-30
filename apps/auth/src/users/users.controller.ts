91775794558c67bf5ce809a6cb201d12243a910d
Nicodav28 <esteveznicolas0@gmail.com>
2024-03-30 12:52:03 -0500

    🔧 fix(auth.controller.spec.ts): comentar la prueba que no se está utilizando
    🔧 fix(auth.controller.ts): eliminar el método getHello() y reemplazarlo por el método login() que utiliza el guard LocalAuthGuard para autenticar al usuario y generar un token JWT
    
    🔧 fix(auth.service.ts): añadir el return del token generado en el método login()
    
    🔧 fix(users.controller.ts): eliminar el método login() ya que se ha movido al controlador AuthController

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,10 +1,6 @@
-import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { LocalAuthGuard } from '../guards/local-auth.guard';
-import { CurrentUser } from '../current-user.decorator';
-import { UserDocument } from './models/users.schema';
-import { Response } from 'express';
 import { AuthService } from '../auth.service';
 
 @Controller('users')
@@ -17,16 +13,5 @@ export class UsersController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(
-    @CurrentUser() user: UserDocument,
-    @Res({ passthrough: true }) response: Response,
-  ) {
-    const login = await this.authService.login(user, response);
-
-    response.send(user);
   }
 }
