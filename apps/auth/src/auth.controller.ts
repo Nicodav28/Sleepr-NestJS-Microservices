76a7f3f8f50ed0453a88992b2edb68bbc967a04a
Nicodav28 <esteveznicolas0@gmail.com>
2024-04-05 22:12:06 -0500

    🔧 fix(auth.controller.ts): cambiar la ruta del controlador a '/auth' 🔧 fix(auth.controller.ts): mover la anotación @UseGuards a la función 'login'

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -5,12 +5,12 @@ import { CurrentUser } from './current-user.decorator';
 import { UserDocument } from './users/models/users.schema';
 import { Response } from 'express';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
+  @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: UserDocument,
     @Res({ passthrough: true }) response: Response,
